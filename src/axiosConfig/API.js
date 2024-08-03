91ee5654de398fb04c25c7ec95a482397bc430ee
IslamAlsayed <eslamalsayed8133@gmail.com>
2024-08-03 20:37:40 +0300

    updated

--- src/axiosConfig/API.js
+++ src/axiosConfig/API.js
@@ -4,33 +4,53 @@ import Cookies from "js-cookie";
 const basicURL = "http://127.0.0.1:8000/api/";
 const AdminToken = Cookies.get("token_foodScan") || null;
 
-const retryRequest = async (requestFunction, retries, delay) => {
-  for (let i = 0; i < retries; i++) {
-    try {
-      return await requestFunction();
-    } catch (error) {
-      if (i < retries - 1) {
-        await new Promise((resolve) => setTimeout(resolve, delay));
-      } else {
-        throw error;
-      }
-    }
-  }
-};
+// const retryRequest = async (requestFunction, retries, delay) => {
+//   for (let i = 0; i < retries; i++) {
+//     try {
+//       return await requestFunction();
+//     } catch (error) {
+//       if (i < retries - 1) {
+//         await new Promise((resolve) => setTimeout(resolve, delay));
+//       } else {
+//         throw error;
+//       }
+//     }
+//   }
+// };
+
+// export const getData = async (url) => {
+//   const request = async () => {
+//     const response = await axios.get(basicURL + url, {
+//       headers: {
+//         Accept: "application/json",
+//         Authorization: `Bearer ${AdminToken}`,
+//       },
+//     });
+//     return response.data.data;
+//   };
+
+//   try {
+//     return await retryRequest(request, 3, 7000);
+//   } catch (error) {
+//     if (error.response) {
+//       console.error("Error response:", error.response.data);
+//     } else {
+//       console.error("Error occurred:", error.message);
+//     }
+//     throw error;
+//   }
+// };
 
 export const getData = async (url) => {
-  const request = async () => {
+  try {
     const response = await axios.get(basicURL + url, {
       headers: {
         Accept: "application/json",
         Authorization: `Bearer ${AdminToken}`,
       },
     });
+    console.log("getData", response.data);
     return response.data.data;
-  };
-
-  try {
-    return await retryRequest(request, 3, 7000);
   } catch (error) {
     if (error.response) {
       console.error("Error response:", error.response.data);
